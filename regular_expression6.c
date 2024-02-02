a45d29e2c9e90c24d13415795715561550008f60
Md. Waseur Rahman <148259013+WaseurGIT@users.noreply.github.com>
2024-02-02 08:41:19 +0600

    Add files via upload

---
+++ regular_expression6.c
@@ -0,0 +1,33 @@
+//Exactly N times [^aeiou]{6}
+#include<stdio.h>
+int main(){
+    int i=0,count=-1,true=0;
+    char x[20];
+    printf("enter a string:");
+    fgets(x,20,stdin);
+    while(x[i]!='\0'){
+        count++;
+        i++;
+    }
+    if(count==6){
+        for(int j=0; j<count-1; j++){
+            if((x[j]=='a' || x[j]=='e' ||x[j]=='i' || x[j]=='o' || x[j]=='u') || (x[j]=='A' || x[j]=='E' || x[j]=='I' || x[j]=='O' || x[j]=='U')){
+                true=1;
+                break;
+            }
+            else{
+                true=0;
+            }
+        }
+        if(true==0){
+            printf("valid\n");
+        }
+        else{
+            printf("invalid\n");
+        }
+    }
+    else{
+        printf("invalid\n");
+    }
+    getch();
+}
